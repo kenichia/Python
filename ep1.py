889853d09872fa6d28aefe02bff4449e19d5cc89
kenichia <kenichia@kenichia-mbpli.pac.adobe.com>
2013-09-06 16:44:32 +0900

    test

---
+++ ep1.py
@@ -0,0 +1,22 @@
+import sys
+
+def calc(names):
+	count = 0
+	lastName = names[0]
+	for i in range(1, len(names)):
+		if lastName > names[i]:
+			count += 1
+		else:
+			lastName = names[i]
+	return count
+
+T = int(sys.stdin.readline())
+for i in range(T):
+	N = int(sys.stdin.readline())
+
+	names = []
+	for j in range(N):
+		names.append(sys.stdin.readline().strip('\r\n'));
+
+	print 'Case #%d: %d' % (i + 1, calc(names))
+
